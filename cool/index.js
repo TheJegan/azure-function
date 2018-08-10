e91cc076915b47e934aea612b4ba8a882ec32099
Jegan Matthews <jematthe@jematthe011579.starbucks.net>
2018-08-10 11:33:26 -0700

    test quotes

--- cool/index.js
+++ cool/index.js
@@ -3,11 +3,17 @@ const randomQuote = require('random-quote');
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     
-   randomQuote()
+    randomQuote()
+    .then(quote => {
+		context.res = {
+			body: "hello " + quote;		
+		};
+	})
+    .catch(err => console.error(err));
 
+/*
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
-            // status: 200, /* Defaults to 200 */
             body: "Hello " + (req.query.name || req.body.name)
         };
     }
@@ -16,6 +22,7 @@ module.exports = function (context, req) {
             status: 400,
             body: "Please pass a name on the query string or in the request body"
         };
-    }
+    }	
     context.done();
+   */
 };
